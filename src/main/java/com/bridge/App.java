7e4a1ba2cd5e113746de808d697fdb24ab6db8e8
Bridge <Brdige@email.com>
2023-09-03 17:30:37 +0800

    master commit

--- src/main/java/com/bridge/App.java
+++ src/main/java/com/bridge/App.java
@@ -1,9 +1,8 @@
 package com.bridge;
-
-
 public class App {
     public static void main( String[] args ) {
         System.out.println( "Hello World!" );
         System.out.println("this is second commit");
+        System.out.println("master branch update");
     }
 }
