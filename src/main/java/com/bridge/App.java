a58720ad6c15a92cc5e6454f9d1fb6931bc05761
Bridge <Brdige@email.com>
2023-09-03 17:24:26 +0800

    second commit

--- src/main/java/com/bridge/App.java
+++ src/main/java/com/bridge/App.java
@@ -4,6 +4,6 @@ package com.bridge;
 public class App {
     public static void main( String[] args ) {
         System.out.println( "Hello World!" );
-
+        System.out.println("this is second commit");
     }
 }
